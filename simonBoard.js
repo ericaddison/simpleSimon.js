e09001cc1af45b8cabd5d5ac29a9957e531cd902
ericaddison <ericaddison@gmail.com>
2016-10-28 08:06:25 -0500

    Encapsulated all board elements into the makeBoard function

--- simonBoard.js
+++ simonBoard.js
@@ -1,112 +1,100 @@
 // depends on circleSector.js
 
-
 /******************************************* 
 * 
 * board object-making function
 *
 *******************************************/
- var makeBoard = function (spec){
+var makeBoard = function (spec){
   spec = spec || {};
   
+  // DOM canvas ID fields
   var canvasID = spec.canvasID || "boardCanvas";
-  var getCanvasID = function() { return canvasID; };
-  var onClick = function(event, mouseDown) { drawBoard(this, event, mouseDown); };
 
+  var onClick = function(event, mouseDown) { draw({event: event, mouseDown: mouseDown}); };
+
+  // geometry values
   var width = spec.width || 300;
   var height = spec.height || 300;
-  return {
-    getCanvasID: getCanvasID,
-    width: width,
-    height: height,
-    onClick: onClick
-  };
- }
+  var centerX = spec.centerX || width/2;
+  var centerY = spec.centerY || height/2;
 
-// default board
- var defaultBoard = makeBoard();
-
-
-
-/******************************************* 
-*
-* function to create the game board canvas that the board
-* will be drawn on
-*
-*******************************************/
- var makeBoardCanvas = function(board, parentElement) {
-  board = board || {};
-  var newCanvas = document.createElement("canvas"); 
-  newCanvas.id = board.getCanvasID() || defaultBoard.getCanvasID();
-  newCanvas.width = board.width || defaultBoard.width;
-  newCanvas.height = board.height || defaultBoard.height;
-  newCanvas.style = "border:1px solid #000000;";
-  newCanvas.onmousedown = function(event){ board.onClick(event, true); };
-  newCanvas.onmouseup = function(event){ board.onClick(event, false); };
-  parentElement.appendChild(newCanvas);
-  return newCanvas;
- }
-
+  // colors for drawing the board
+  var onColors = ["#FF0000", "#00FF00", "#3344FF", "#FFA500"];
+  var offColors = ["#BB0000", "#00BB00", "#3040BB", "#BB8500"];
 
 
 /******************************************* 
 *
 * Draw the game board
 *
+* Useful fields for the drawSpec object:
+* 	{ 	canvas: the canvas on which to draw the board
+*   	centerX: the center X coordinate, in canvas coords
+*		centerY: the center Y coordinate, in canvas coords
+*		event: optional mouse event
+*		mouseDown: optional boolean
+*					true=> mouseDown event, false=> mouseUp event
+*	}
 *******************************************/
- var drawBoard = function(board, e, mouseDown) {
-  board = board || defaultBoard;
-  var canvas = document.getElementById(board.getCanvasID());
-  var ctx = canvas.getContext("2d");
+  var draw = function(drawSpec) {
+   drawSpec = drawSpec || {};
+   var canvas = drawSpec.canvas || document.getElementById(canvasID);
+   var e = drawSpec.event;
+   var mouseDown = drawSpec.mouseDown;
 
-  if ( e && mouseDown ) {
-    var rect = canvas.getBoundingClientRect();
-    var mouseX = e.clientX - rect.left;
-    var mouseY = e.clientY - rect.top;
-  }
+   // update center coordinates
+   centerX = drawSpec.centerX || centerX;
+   centerY = drawSpec.centerY || centerY;
 
-  // board values
-  var boardSize = Math.min(board.width, board.height);
-  var bigRadius = 0.9*boardSize/2;
-  var innerButtonRadius = 0.25*boardSize/2;
-  var outerButtonRadius = 0.7*boardSize/2;
-  var centerX = board.width/2;
-  var centerY = board.height/2;
-  var sectorSpacing = bigRadius*0.05;
+   var ctx = canvas.getContext("2d");
+   if ( e && mouseDown ) {
+     var rect = canvas.getBoundingClientRect();
+     var mouseX = e.clientX - rect.left;
+     var mouseY = e.clientY - rect.top;
+   }
 
-  var onColors = ["#FF0000", "#00FF00", "#3344FF", "#FFA500"];
-  var offColors = ["#BB0000", "#00BB00", "#3040BB", "#BB8500"];
-  var buttonInfo = {centerX: centerX,
+   // board values
+   var boardSize = Math.min(width, height);
+   var bigRadius = 0.9*boardSize/2;
+   var innerButtonRadius = 0.25*boardSize/2;
+   var outerButtonRadius = 0.7*boardSize/2;
+
+   var sectorSpacing = bigRadius*0.05;
+
+   var buttonInfo = {centerX: centerX,
                     centerY: centerY,
                     innerRadius: innerButtonRadius,
                     outerRadius: outerButtonRadius,
                     sectorSpacing: sectorSpacing};
 
-  // draw center black circle
-  var gradient = ctx.createRadialGradient(centerX,centerY,0,centerX,centerY,bigRadius);
-  gradient.addColorStop(0.95,"#303030");
-  gradient.addColorStop(1,"#DDDDDD");
-  ctx.fillStyle = gradient;
-  ctx.arc(centerX, centerY, bigRadius, 0, 2*Math.PI);
-  ctx.fill();
+   // draw center black circle
+   var gradient = ctx.createRadialGradient(centerX,centerY,0,centerX,centerY,bigRadius);
+   gradient.addColorStop(0.95,"#303030");
+   gradient.addColorStop(1,"#DDDDDD");
+   ctx.fillStyle = gradient;
+   ctx.arc(centerX, centerY, bigRadius, 0, 2*Math.PI);
+   ctx.fill();
 
-  // draw the Buttons
+   // draw the Buttons
 
-  for( iButton=0; iButton<4; iButton++ ){
-    buttonInfo.buttonNumber = iButton;
-    computeButtonPath(ctx, buttonInfo);
+   for( iButton=0; iButton<4; iButton++ ){
+     buttonInfo.buttonNumber = iButton;
+     computeButtonPath(ctx, buttonInfo);
 
-    buttonInfo.on = ( e && mouseDown && ctx.isPointInPath(mouseX, mouseY) );
-    buttonInfo.color = (buttonInfo.on) ? onColors[iButton] : offColors[iButton];
+     buttonInfo.on = ( e && mouseDown && ctx.isPointInPath(mouseX, mouseY) );
+     buttonInfo.color = (buttonInfo.on) ? onColors[iButton] : offColors[iButton];
 
-    ctx.fillStyle = (buttonInfo.on) ? getOnButtonGradient(ctx, buttonInfo) 
-                                  : getOffButtonGradient(ctx, buttonInfo);
-    ctx.fill();
+     ctx.fillStyle = (buttonInfo.on) ? getOnButtonGradient(ctx, buttonInfo) 
+                                  	 : getOffButtonGradient(ctx, buttonInfo);
+     ctx.fill();
+
+   }
+
 
   }
 
 
- }
 
 
 /******************************************* 
@@ -114,7 +102,7 @@
 * Get the gradient needed for drawing an ON button (pressed button)
 *
 *******************************************/
- var getOnButtonGradient = function(ctx, buttonInfo) {
+  var getOnButtonGradient = function(ctx, buttonInfo) {
     var halfRadius = (buttonInfo.innerRadius + buttonInfo.outerRadius)/2.5;
     var xSign = Math.pow(-1, buttonInfo.buttonNumber%3>0);
     var ySign = Math.pow(-1, buttonInfo.buttonNumber>=2 );
@@ -134,7 +122,7 @@
     gradient.addColorStop(0,"white");
 
     return gradient;
- }
+  }
 
 
 
@@ -143,7 +131,7 @@
 * Get the gradient needed for drawing an OFF button (unpressed button)
 *
 *******************************************/
- var getOffButtonGradient = function(ctx, buttonInfo) {
+  var getOffButtonGradient = function(ctx, buttonInfo) {
     var gradCenterX = buttonInfo.centerX;
     var gradCenterY = buttonInfo.centerY;
 
@@ -159,7 +147,7 @@
     gradient.addColorStop(1,"white");
 
     return gradient;
- }
+  }
 
 
 /******************************************* 
@@ -167,7 +155,7 @@
 * Get the path for a button, but don't fill
 *
 *******************************************/
- var computeButtonPath = function(ctx, buttonInfo) {
+  var computeButtonPath = function(ctx, buttonInfo) {
     var xSign = Math.pow(-1, buttonInfo.buttonNumber%3>0);
     var ySign = Math.pow(-1, buttonInfo.buttonNumber>=2 );
     var startAngle = 90*buttonInfo.buttonNumber;
@@ -178,5 +166,17 @@
       buttonInfo.centerY+buttonInfo.sectorSpacing*ySign, 
       buttonInfo.innerRadius, buttonInfo.outerRadius,
       startAngle, endAngle);
- }
+  }
+
+
+
+// Return a new anonymous object with all public fields/methods information
+ return {
+   canvasID: canvasID,
+   draw: draw,
+   width: width,
+   height: height,
+   onClick: onClick
+ };
+}
 
